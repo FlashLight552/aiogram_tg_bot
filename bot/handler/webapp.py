fa6e2009bcf8e2dd7350680fa7aebdfc288b0d17
FlashLight552 <saw1997hunterone@gmail.com>
2022-08-04 12:29:37 +0300

    Update webapp.py

--- bot/handler/webapp.py
+++ bot/handler/webapp.py
@@ -4,23 +4,45 @@ import requests
 async def web_app_mess(message: types.message):
     await message.delete()
     date = str(message.web_app_data.data).split('-')
+    mounth_name = {
+    "Nisan": "Нисан",
+    "Iyyar": "Ияр",
+    "Sivan": "Сиван",
+    "Tamuz": "Тамуз",
+    "Av": "Ав",
+    "Elul": "Элул",
+    "Tishrei": "Тишрей",
+    "Cheshvan": "Хешван",
+    "Kislev": "Кислев",
+    "Tevet": "Тевет",
+    "Sh'vat": "Шват",
+    "Adar": "Адар",
+    "Adar I": "Адар I",
+    "Adar II": "Адар II",
+    }
     if date[0] == 'gregorian':
         date[4] = str(date[4]).replace('true','on')
         url = f'https://www.hebcal.com/converter?cfg=json&date={date[3]}-{date[2]}-{date[1]}&g2h=1&strict=1&gs={date[4]}'
         raw_request = requests.get(url)
         text_request = raw_request.json()
-        
-        await message.answer("Gregorian data:\n"\
+        for item in mounth_name.keys():
+            if item == text_request['hm']:
+                hm =  mounth_name[item]
+
+        await message.answer("Григорианская дата:\n"\
                         f"{text_request['gd']}/{text_request['gm']}/{text_request['gy']}\n"\
-                        "Hebrew data:\n"\
-                        f"{text_request['hd']} {text_request['hm']} {text_request['hy']}")
+                        "Еврейская дата:\n"\
+                        f"{text_request['hd']} {hm} {text_request['hy']}")
 
     if date[0] == 'hebrew':
         url = f'https://www.hebcal.com/converter?cfg=json&hy={date[3]}&hm={date[2]}&hd={date[1]}&h2g=1&strict=1'
         request = requests.get(url).json()
-        await message.answer("Hebrew data:\n"\
-                        f"{request['hd']} {request['hm']} {request['hy']}\n"\
-                        "Gregorian data:\n"\
+        for item in mounth_name.keys():
+            if item == text_request['hm']:
+                hm =  mounth_name[item]
+        await message.answer("Еврейская дата:\n"\
+                        f"{request['hd']} {hm} {request['hy']}\n"\
+                        "Григорианская дата:\n"\
                         f"{request['gd']}/{request['gm']}/{request['gy']}")
 
 
